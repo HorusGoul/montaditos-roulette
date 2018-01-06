c7feb039757aee5b1fdb9e08cc7a530533bb8deb
Horus Lugo <horusgoul@gmail.com>
2018-01-06 04:19:34 +0100

    Adds basename and test script in travis.yml

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/" component={MainMenu} />
           <Route
